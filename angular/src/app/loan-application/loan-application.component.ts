a9bbbb1f3559e9384a22b22ba6f655e6d93f1654
smasadhaider <smasadhaider@gmail.com>
2020-09-07 00:30:35 +0500

    loan application

---
+++ angular/src/app/loan-application/loan-application.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-loan-application',
+    templateUrl: './loan-application.component.html',
+    styleUrls: ['./loan-application.component.css']
+})
+export class LoanApplicationComponent implements OnInit {
+
+    loanApplication: any = {};
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
