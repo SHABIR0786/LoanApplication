08465e82f542d374cd5521eb7888d6ee69f2670f
Faraz Ahmed Shaikh <farazahmed.shaikh@technologyplex.com>
2020-09-13 13:29:53 -0700

    Add-Table-Borrower&Co-Information&EmploymenInformation

---
+++ aspnet-core/src/LoanManagement.Core/Borrower-Information/BorrowerInformation.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LoanManagement.Borrower_Information
+{
+    public class BorrowerInformation : FullAuditedEntity<long>, IMayHaveTenant
+    {
+        [Required]
+        [StringLength(100)]
+        public string BorrowersName { get; set; }
+
+        [StringLength(100)]
+        public string SocialSecurityNumber { get; set; }
+        public string HomePhone { get; set; }
+        public DateTime DOB { get; set; }
+        public int? YearsSchool { get; set; }
+        public string Marital { get; set; }
+        public string PresentAddress { get; set; }
+        public string PresentAddressType { get; set; }
+        public int? PresentAddressNoOfYears { get; set; }
+
+        public string MailingAddress { get; set; }
+        public string FormerAddressModel { get; set; }
+
+        public string FormerAddressType { get; set; }
+        public int? FormerAddressNoOfYears { get; set; }
+
+        public int? TenantId { get; set; }
+    }
+}
