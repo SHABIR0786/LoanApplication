b5b217fd1143d5bc3953b7a5bdbaf98127a1b1cf
Faraz Ahmed Shaikh <farazahmed.shaikh@technologyplex.com>
2020-09-15 15:31:24 -0700

    Step 4: Borrower EMployment Information Service

---
+++ aspnet-core/src/LoanManagement.Application/Borrower-Employment-Information/Dto/BorrowerEmploymentInformationInputDto.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoanManagement.BorrowerEmploymentInformations.Dto
+{
+    public class BorrowerEmploymentInformationInputDto
+    {
+        public string EmployersName1 { get; set; }
+
+    }
+}
---
+++ aspnet-core/src/LoanManagement.Application/Borrower-Employment-Information/Dto/PagedBorrowerEmploymentInformationDtoResultRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace LoanManagement.BorrowerEmploymentInformations.Dto
+{
+    public class PagedBorrowerEmploymentInformationDtoResultRequestDto : PagedResultRequestDto
+    {
+         public string Keyword { get; set; }
+         public int? TenantId { get; set; }
+    }
+}
+
