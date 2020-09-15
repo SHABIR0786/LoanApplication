0b0b787e7a1cee292c2cb4a698996fbf55c5458a
KhalilMohammad <khalilmohammadmirza@gmail.com>
2020-09-16 00:02:17 +0500

    Move to maria db instead of sql server

--- aspnet-core/src/LoanManagement.EntityFrameworkCore/EntityFrameworkCore/LoanManagementDbContextConfigurer.cs
+++ aspnet-core/src/LoanManagement.EntityFrameworkCore/EntityFrameworkCore/LoanManagementDbContextConfigurer.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
 
 namespace LoanManagement.EntityFrameworkCore
 {
     public static class LoanManagementDbContextConfigurer
     {
-        public static void Configure(DbContextOptionsBuilder<LoanManagementDbContext> builder, string connectionString)
+        public static void Configure(DbContextOptionsBuilder<LoanManagementDbContext> builder, string connectionString, IWebHostEnvironment webHostEnvironment = null)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString, mySqlOptions =>
+            {
+                mySqlOptions.ServerVersion(new Version(10, 3, 17), ServerType.MariaDb);
+                mySqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 10,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+
+            if(webHostEnvironment == null || webHostEnvironment.IsDevelopment())
+            {
+                builder.EnableSensitiveDataLogging();
+                builder.EnableDetailedErrors();
+                builder.ConfigureWarnings(option => option.Throw());
+            }
         }
 
-        public static void Configure(DbContextOptionsBuilder<LoanManagementDbContext> builder, DbConnection connection)
+        public static void Configure(DbContextOptionsBuilder<LoanManagementDbContext> builder, DbConnection connection, IWebHostEnvironment webHostEnvironment = null)
         {
-            builder.UseSqlServer(connection);
+            builder.UseMySql(connection, mySqlOptions =>
+            {
+                mySqlOptions.ServerVersion(new Version(10, 3, 17), ServerType.MariaDb);
+                mySqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 10,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+
+            if (webHostEnvironment == null || webHostEnvironment.IsDevelopment())
+            {
+                builder.EnableSensitiveDataLogging();
+                builder.EnableDetailedErrors();
+                builder.ConfigureWarnings(option => option.Throw());
+            }
         }
     }
 }
