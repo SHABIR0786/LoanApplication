66ef30f63b29f88864664780d60f465808bb9817
smasadhaider <smasadhaider@gmail.com>
2020-09-18 03:50:21 +0500

    Frontend integration and code cleanup

---
+++ angular/src/app/loan-application/employment-information/employment-information.component.ts
@@ -0,0 +1,25 @@
+import {Component, DoCheck, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+    selector: 'app-employment-information',
+    templateUrl: './employment-information.component.html',
+    styleUrls: ['./employment-information.component.css']
+})
+export class EmploymentInformationComponent implements OnInit, DoCheck {
+
+    @Input() heading = 'Borrower';
+    @Input() data: any = {};
+    @Input() showHeading: boolean = false;
+    @Input() secondary: boolean = false;
+    @Output() onDataChange: EventEmitter<any> = new EventEmitter<any>();
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    ngDoCheck() {
+        this.onDataChange.next(this.data);
+    }
+}
