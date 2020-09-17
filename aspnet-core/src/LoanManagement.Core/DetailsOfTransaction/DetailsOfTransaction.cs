35af91efb5bb2fad6908f7b3f532c4ce1d3116f1
Faraz Ahmed Shaikh <farazahmed.shaikh@technologyplex.com>
2020-09-17 14:07:16 -0700

    Step7: Table Details of transactions

---
+++ aspnet-core/src/LoanManagement.Core/DetailsOfTransaction/DetailsOfTransaction.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManagement.DetailsOfTransactions
+{
+    public class DetailsOfTransaction : FullAuditedEntity<long>, IMayHaveTenant
+    {
+        public decimal? PurchasePrice { get; set; }
+        public decimal? Alterations { get; set; }
+        public decimal? Land { get; set; }
+        public decimal? Refinance { get; set; }
+        public decimal? EstimatedPreparedItem { get; set; }
+        public decimal? EstimatedClosingCost { get; set; }
+        public decimal? FundingFee { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal? TotalCost { get; set; }
+        public decimal? SubOrdinateFinancing { get; set; }
+        public decimal? BorrowersClosingCost { get; set; }
+        public decimal? OtherCredits { get; set; }
+        public decimal? LoanAmount { get; set; }
+        public decimal? CashFrom { get; set; }
+        public int? TenantId { get; set; }
+    }
+}
