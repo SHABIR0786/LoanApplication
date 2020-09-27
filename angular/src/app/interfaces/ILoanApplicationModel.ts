02c255e003dcaac7e83fedcc3b95fc2deefd73c3
smasadhaider <smasadhaider@gmail.com>
2020-09-27 21:38:52 +0500

    Step 5,6,7 done

--- angular/src/app/interfaces/ILoanApplicationModel.ts
+++ angular/src/app/interfaces/ILoanApplicationModel.ts
@@ -1,7 +1,13 @@
 import {ILoanDetailModel} from './ILoanDetailModel';
 import {IPersonalDetailModel} from './IPersonalDetailModel';
+import {IAdditionalDetailModel} from './IAdditionalDetailModel';
+import {IConsentDetailModel} from './IConsentDetailModel';
+import {ICreditAuthAgreementModel} from './ICreditAuthAgreementModel';
 
 export interface ILoanApplicationModel {
     loanDetails?: ILoanDetailModel;
-    personalDetails?: IPersonalDetailModel;
+    personalInformation?: IPersonalDetailModel;
+    orderCredit?: ICreditAuthAgreementModel;
+    additionalDetails?: IAdditionalDetailModel;
+    eConsent?: IConsentDetailModel;
 }
