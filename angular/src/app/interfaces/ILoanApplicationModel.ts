08325fc091e9aa12b6641feef7bffc8887b2acf1
smasadhaider <smasadhaider@gmail.com>
2020-09-28 12:11:50 +0500

    step 3 bug fixes

--- angular/src/app/interfaces/ILoanApplicationModel.ts
+++ angular/src/app/interfaces/ILoanApplicationModel.ts
@@ -3,10 +3,12 @@ import {IPersonalDetailModel} from './IPersonalDetailModel';
 import {IAdditionalDetailModel} from './IAdditionalDetailModel';
 import {IConsentDetailModel} from './IConsentDetailModel';
 import {ICreditAuthAgreementModel} from './ICreditAuthAgreementModel';
+import {IExpenseModel} from './IExpenseModel';
 
 export interface ILoanApplicationModel {
     loanDetails?: ILoanDetailModel;
     personalInformation?: IPersonalDetailModel;
+    expenses?: IExpenseModel;
     orderCredit?: ICreditAuthAgreementModel;
     additionalDetails?: IAdditionalDetailModel;
     eConsent?: IConsentDetailModel;
--- angular/src/app/loan-application/expenses/expenses.component.ts
+++ angular/src/app/loan-application/expenses/expenses.component.ts
@@ -1,45 +1,76 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { IExpenseModel } from '@app/interfaces/IExpenseModel';
+import {Component, DoCheck, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {IExpenseModel} from '@app/interfaces/IExpenseModel';
+
 @Component({
-  selector: 'app-expenses',
-  templateUrl: './expenses.component.html',
-  styleUrls: ['./expenses.component.css']
+    selector: 'app-expenses',
+    templateUrl: './expenses.component.html',
+    styleUrls: ['./expenses.component.css']
 })
-export class ExpensesComponent implements OnInit {
+export class ExpensesComponent implements OnInit, DoCheck {
 
-  //id = Math.random().toString(36).substring(2);
-  @Input() heading: string;
-  @Input() form: FormGroup;
-  @Input() data: IExpenseModel;
-  @Output() onDataChange: EventEmitter<any> = new EventEmitter<any>();
+    @Input() data: IExpenseModel = {};
+    @Output() onDataChange: EventEmitter<any> = new EventEmitter<any>();
 
+    form: FormGroup;
 
+    constructor() {
+    }
 
+    ngOnInit(): void {
+        this.initForm();
+    }
 
-  constructor() { }
+    ngDoCheck() {
+        this.data = this.form.value;
+        if (this.form.valid) {
+            this.onDataChange.next(this.form.value);
+        }
+    }
 
-  ngOnInit(): void {
-  }
+    initForm() {
+        this.form = new FormGroup({
+            isLiveWithFamilySelectRent: new FormControl(this.data.isLiveWithFamilySelectRent),
+            rent: new FormControl(this.data.rent, [Validators.required]),
+            otherHousingExpenses: new FormControl(this.data.otherHousingExpenses, [Validators.required]),
+            firstMortgage: new FormControl(this.data.firstMortgage, [Validators.required]),
+            secondMortgage: new FormControl(this.data.secondMortgage),
+            hazardInsurance: new FormControl(this.data.hazardInsurance),
+            realEstateTaxes: new FormControl(this.data.realEstateTaxes, [Validators.required]),
+            mortgageInsurance: new FormControl(this.data.mortgageInsurance, [Validators.required]),
+            homeOwnersAssociation: new FormControl(this.data.homeOwnersAssociation, [Validators.required])
+        });
 
-  initForm() {
-    this.form = new FormGroup({
-      isLiveWithFamilySelectRent: new FormControl(this.data.isLiveWithFamilySelectRent),
-      rent: new FormControl(this.data.rent, [Validators.required]),
-      otherHousingExpenses: new FormControl(this.data.otherHousingExpenses, [Validators.required]),
-      firstMortgage: new FormControl(this.data.firstMortgage, [Validators.required]),
-      secondMortgage: new FormControl(this.data.secondMortgage),
-      hazardInsurance: new FormControl(this.data.hazardInsurance),
-      realEstateTaxes: new FormControl(this.data.realEstateTaxes, [Validators.required]),
-      mortgageInsurance: new FormControl(this.data.mortgageInsurance, [Validators.required]),
-      homeownersAssociation: new FormControl(this.data.homeownersAssociation, [Validators.required])
-    });
-  }
+        this.form.get('isLiveWithFamilySelectRent').valueChanges.subscribe(isLiveWithFamilySelectRent => {
+            if (isLiveWithFamilySelectRent) {
+                this.form.get('rent').setValidators([Validators.required]);
 
-  onRentChange() {
-    debugger;
-    this.data;
+                this.form.get('firstMortgage').setValue(null);
+                this.form.get('firstMortgage').setValidators(null);
+                this.form.get('realEstateTaxes').setValue(null);
+                this.form.get('realEstateTaxes').setValidators(null);
+                this.form.get('mortgageInsurance').setValue(null);
+                this.form.get('mortgageInsurance').setValidators(null);
+                this.form.get('homeOwnersAssociation').setValue(null);
+                this.form.get('homeOwnersAssociation').setValidators(null);
 
-  }
+                this.form.get('secondMortgage').setValue(null);
+                this.form.get('hazardInsurance').setValue(null);
 
+            } else {
+                this.form.get('rent').setValue(null);
+                this.form.get('rent').setValidators(null);
+
+                this.form.get('firstMortgage').setValidators([Validators.required]);
+                this.form.get('realEstateTaxes').setValidators([Validators.required]);
+                this.form.get('mortgageInsurance').setValidators([Validators.required]);
+                this.form.get('homeOwnersAssociation').setValidators([Validators.required]);
+            }
+            this.form.get('rent').updateValueAndValidity();
+            this.form.get('firstMortgage').updateValueAndValidity();
+            this.form.get('realEstateTaxes').updateValueAndValidity();
+            this.form.get('mortgageInsurance').updateValueAndValidity();
+            this.form.get('homeOwnersAssociation').updateValueAndValidity();
+        });
+    }
 }
