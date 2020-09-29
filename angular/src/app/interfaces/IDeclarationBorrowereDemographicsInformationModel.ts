9070fd527c2d2471d20c0a93178c9127a6793600
Faraz Ahmed Shaikh <farazahmed.shaikh@technologyplex.com>
2020-09-29 13:30:13 -0700

    step 8 decalaration2

---
+++ angular/src/app/interfaces/IDeclarationBorrowereDemographicsInformationModel.ts
@@ -0,0 +1,33 @@
+export interface IDeclarationBorrowereDemographicsInformationModel {
+    id?: number;
+    isHispanicOrLatino?: boolean;
+    isMexican?: boolean;
+    isPuertoRican?: boolean;
+    isCuban?: boolean;
+    isOtherHispanicOrLatino?: boolean;
+    origin?: string;
+    isNotHispanicOrLatino?: boolean;
+    isNotProvideInformation?: boolean;
+    isAmericanIndianOrAlaskaNative?: boolean;
+    nameOfEnrolledOrPrincipalTribe?: string;
+    isAsian?: boolean;
+    isAsianIndian?: boolean;
+    isChinese?: boolean;
+    isFilipino?: boolean;
+    isJapanese?: boolean;
+    isKorean?: boolean;
+    isVietnamese?: boolean;
+    isOtherAsian?: boolean;
+    enterRace?: string;
+    isBlackOrAfricanAmerican?: boolean;
+    isNativeHawaiianOrOtherPacificIslander?: boolean;
+    isNativeHawaiian?: boolean;
+    isGuamanianOrChamorro?: boolean;
+    isOtherPacificIslander?: boolean;
+    EnterRace2?: string;
+    isWhite?: boolean;
+    isWishToprovideInformation?: boolean;
+    isMale?: boolean;
+    isFeeMale?: boolean;
+    isDonotProvideSexInformattion?: boolean;
+}
