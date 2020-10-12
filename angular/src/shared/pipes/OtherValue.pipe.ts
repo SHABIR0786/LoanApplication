6819e1fc3035db0c4a46cde636d71a3995ab8cac
MQasimRaza <mqasimraza11@gmail.com>
2020-10-12 07:37:26 +0500

    OtherValuePipeChanges

--- angular/src/shared/pipes/OtherValue.pipe.ts
+++ angular/src/shared/pipes/OtherValue.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({
   name: "otherValue",
 })
-export class EthnicityOtherValue implements PipeTransform {
+export class EthnicityOtherValuePipe implements PipeTransform {
   transform(value: any[], id: number): string {
     if (value === undefined || value === null || value.length === 0)
       return "--";
