c347ff7149daa7fea4f11ceb3c5f67867f19df92
MQasimRaza <mqasimraza11@gmail.com>
2020-10-12 07:31:31 +0500

    AdditionalIncomeModel Changes

--- angular/src/app/interfaces/IAdditionalIncomeModel.ts
+++ angular/src/app/interfaces/IAdditionalIncomeModel.ts
@@ -2,8 +2,10 @@ import { IBorrowerEmploymentInfoModel } from "./IBorrowerEmploymentInfoModel";
 import { IBorrowerMonthlyIncomeModel } from "./IBorrowerMonthlyIncomeModel";
 
 export interface IAdditionalIncomeModel {
+
   id?: number;
   borrowerTypeId?: number;
   incomeSourceId?: number;
   amount?: number;
+  incomeSourceIdName?: string;
 }
