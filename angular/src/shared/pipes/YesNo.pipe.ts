c600a70f9fea31cc93fe25e22074d0734f3b561a
MQasimRaza <mqasimraza11@gmail.com>
2020-10-12 07:37:49 +0500

    YesNoPipeChanges

--- angular/src/shared/pipes/YesNo.pipe.ts
+++ angular/src/shared/pipes/YesNo.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({
   name: "yesNo",
 })
-export class EthnicityYesNo implements PipeTransform {
+export class EthnicityYesNoPipe implements PipeTransform {
   transform(value: any[], id: number): string {
     if (value === undefined || value === null || value.length === 0)
       return "--";
