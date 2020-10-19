83fb73b9422c08edd1ff136e22575edec99cb859
izhankhan69 <izhank62@gmail.com>
2020-10-20 03:12:37 +0500

    Co borrower residential address back end

--- aspnet-core/src/LoanManagement.Core/ViewModels/PersonalInformationDto.cs
+++ aspnet-core/src/LoanManagement.Core/ViewModels/PersonalInformationDto.cs
@@ -15,12 +15,11 @@ namespace LoanManagement.ViewModels
         public BorrowerDto CoBorrower { get; set; }
 
         public AddressDto ResidentialAddress { get; set; }
+
+        public AddressDto CoBorrowerResidentialAddress { get; set; }
         public List<AddressDto> PreviousAddresses { get; set; }
         public AddressDto MailingAddress { get; set; }
         public bool? IsMailingAddressSameAsResidential { get; set; }
-
-
-        public AddressDto CoBorrowerResidentialAddress { get; set; }
         public List<AddressDto> CoBorrowerPreviousAddresses { get; set; }
         public AddressDto CoBorrowerMailingAddress { get; set; }
         public bool? CoBorrowerIsMailingAddressSameAsResidential { get; set; }
