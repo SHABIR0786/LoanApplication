3f380eb08114dbe04d741a23478fb934be0ffaaf
izhankhan69 <izhank62@gmail.com>
2020-10-21 02:27:08 +0500

    Add Individual Request

---
+++ aspnet-core/src/LoanManagement.Core/CoreLogicModels/IndividualRequest/LoanApplication.cs
@@ -0,0 +1,9 @@
+using System.Xml.Serialization;
+
+namespace LoanManagement.CoreLogicModels.IndividualRequest{
+
+[XmlRoot(ElementName="LOAN_APPLICATION")]
+	public class LoanApplication {
+		[XmlElement(ElementName="BORROWER")]
+		public Borrower BORROWER { get; set; }
+	}}
