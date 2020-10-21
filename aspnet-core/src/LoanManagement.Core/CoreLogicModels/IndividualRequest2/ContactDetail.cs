9d59552cea68a70f79f0678c0c349ba8da8bae5e
izhankhan69 <izhank62@gmail.com>
2020-10-22 00:39:35 +0500

    Add Individual Request 2

---
+++ aspnet-core/src/LoanManagement.Core/CoreLogicModels/IndividualRequest2/ContactDetail.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace LoanManagement.CoreLogicModels.IndividualRequest2{
+
+[XmlRoot(ElementName="CONTACT_DETAIL")]
+	public class ContactDetail {
+		[XmlElement(ElementName="CONTACT_POINT")]
+		public List<ContactPoint> CONTACT_POINT { get; set; }
+		[XmlAttribute(AttributeName="_Name")]
+		public string _Name { get; set; }
+	}
+}
---
+++ aspnet-core/src/LoanManagement.Core/CoreLogicModels/IndividualRequest2/Creditor.cs
@@ -0,0 +1,10 @@
+
+using System.Xml.Serialization;
+
+namespace LoanManagement.CoreLogicModels.IndividualRequest2{
+	[XmlRoot(ElementName="_CREDITOR")]
+	public class Creditor {
+		[XmlAttribute(AttributeName="_Name")]
+		public string _Name { get; set; }
+	}
+}
