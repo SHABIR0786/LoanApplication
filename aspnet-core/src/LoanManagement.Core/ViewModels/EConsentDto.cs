5329b62d1785bd215c745974e5230e1b028c3a9c
KhalilMohammad <mqasimraza11@gmail.com>
2020-10-21 09:10:33 +0500

    Add Missing Coborrower

--- aspnet-core/src/LoanManagement.Core/ViewModels/EConsentDto.cs
+++ aspnet-core/src/LoanManagement.Core/ViewModels/EConsentDto.cs
@@ -8,5 +8,9 @@ namespace LoanManagement.ViewModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public bool? CoborrowerAgreeEConsent { get; set; }
+        public string CoborrowerFirstName { get; set; }
+        public string CoborrowerLastName { get; set; }
+        public string CoborrowerEmail { get; set; }
     }
 }
