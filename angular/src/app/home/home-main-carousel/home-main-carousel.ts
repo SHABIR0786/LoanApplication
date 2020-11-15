9af23a94c387444b13a9ded264ea44d410483ba6
MQasimRaza <mqasimraza11@gmail.com>
2020-11-15 23:23:59 +0500

    Add Carousel

---
+++ angular/src/app/home/home-main-carousel/home-main-carousel.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "./home-main-carousel.html",
+  styleUrls: ["./home-main-carousel.css"],
+  selector: "home-main-carousel",
+})
+export class HomeMainCarousel {
+  // buyingAHome: string = "Buying A home Guide";
+}
