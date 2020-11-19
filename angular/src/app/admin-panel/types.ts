500acae274aa42d1c445048f495852041b686d3a
MQasimRaza <mqasimraza11@gmail.com>
2020-11-19 06:55:32 +0500

    Add Interfaces

---
+++ angular/src/app/admin-panel/types.ts
@@ -0,0 +1,39 @@
+export interface IHomePage {
+  // Top Carousel
+  sliders?: [];
+  header?: string;
+  subHeader?: string;
+  // Featured Mortgage Options Section 1
+  image?: string;
+  topCaptiom1?: string;
+  heading1?: string;
+  bottomCaption1?: string;
+  // Featured Mortgage Options Section 2
+  image1?: string;
+  topCaptiom2?: string;
+  heading2?: string;
+  bottomCaption2?: string;
+  // Featured Mortgage Options Section 2 Sub Section 1
+  image2?: string;
+  topCaptiom3?: string;
+  heading3?: string;
+  // Featured Mortgage Options Section 2 Sub Section 2
+  image3?: string;
+  topCaptiom4?: string;
+  heading4?: string;
+  // Your Independent Mortgage Broker In California Section
+  video?: string;
+  // Tips For Getting A Home Mortgage In California Section 1
+  mainHeading?: string;
+  subMainHeading?: string;
+  subHeading1?: string;
+  subHeading2?: string;
+  subHeading3?: string;
+  subHeading4?: string;
+  // Tips For Getting A Home Mortgage In California Section 2
+  image4?: string;
+  paragraph?: string;
+  // Our Customer Says
+  comments?: [];
+  names?: [];
+}
