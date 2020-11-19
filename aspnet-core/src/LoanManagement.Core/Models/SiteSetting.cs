1f126fe9b6d7f206a5adee7798c442c63cb5a635
KhalilMohammad <khalilmohammadmirza@gmail.com>
2020-11-19 23:54:13 +0500

    Add sitesettings

---
+++ aspnet-core/src/LoanManagement.Core/Models/SiteSetting.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Domain.Entities.Auditing;
+
+namespace LoanManagement.Models
+{
+    public class SiteSetting : FullAuditedEntity<int>
+    {
+        public string PageIdentifier { get; set; }
+        public string PageSetting { get; set; }
+    }
+}
