60618d971d12ff619c66b0d766f964ecd0532189
MQasimRaza <mqasimraza11@gmail.com>
2020-11-20 06:39:49 +0500

    Backend

--- aspnet-core/src/LoanManagement.Core/Models/SiteSetting.cs
+++ aspnet-core/src/LoanManagement.Core/Models/SiteSetting.cs
@@ -5,6 +5,7 @@ namespace LoanManagement.Models
     public class SiteSetting : FullAuditedEntity<int>
     {
         public string PageIdentifier { get; set; }
+        public string PageName { get; set; }
         public string PageSetting { get; set; }
     }
 }
