3c9d3bc541516276a5aac53bb77eed39853e6cf1
MQasimRaza <mqasimraza11@gmail.com>
2020-11-20 06:41:53 +0500

    Add Buttons, FormGroup And FormArray

--- angular/src/app/admin-panel/admin-panel.component.ts
+++ angular/src/app/admin-panel/admin-panel.component.ts
@@ -1,6 +1,13 @@
 import { Component, DoCheck, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
-import { IHomePage } from "./types";
+import { FormArray, FormControl, FormGroup } from "@angular/forms";
+import { from } from "rxjs";
+import {
+  IFeaturedMortgage,
+  IHomePage,
+  IMainCarousel,
+  ITestimonial,
+  ITipsForGettingAHomeMortgage,
+} from "./types";
 
 @Component({
   selector: "app-admin-panel",
@@ -11,21 +18,54 @@ export class AdminPanelComponent implements OnInit, DoCheck {
   pages = [];
   currentPage?: number = null;
   HomePageForm: FormGroup;
-  homePage: IHomePage = {};
+  homePage: IHomePage = {
+    mainCarousels: [{}],
+    featuredMortgages: [{}, {}],
+    tipsForGettingAHomeMortgages: [{}, {}, {}, {}],
+    testimonials: [{}],
+  };
+
+  get testimonials(): FormArray {
+    return this.HomePageForm.get("testimonials") as FormArray;
+  }
+
+  get tipsForGettingAHomeMortgages(): FormArray {
+    return this.HomePageForm.get("tipsForGettingAHomeMortgages") as FormArray;
+  }
+
+  get featuredMortgages(): FormArray {
+    return this.HomePageForm.get("featuredMortgages") as FormArray;
+  }
+
+  get mainCarousels(): FormArray {
+    return this.HomePageForm.get("mainCarousels") as FormArray;
+  }
 
   ngOnInit(): void {
     this.pages = [
       {
         id: 1,
-        name: "Home page",
+        name: "Home Page",
         path: "app/home",
       },
     ];
 
     this.HomePageForm = new FormGroup({
-      sliders: new FormControl(this.homePage.sliders),
-      header: new FormControl(this.homePage.header),
-      subHeader: new FormControl(this.homePage.subHeader),
+      testimonials: new FormArray(
+        this.homePage.testimonials.map((d) => this.initTestimonials(d))
+      ),
+
+      tipsForGettingAHomeMortgages: new FormArray(
+        this.homePage.tipsForGettingAHomeMortgages.map((d) =>
+          this.initTipsForGettingAHomeMortgages(d)
+        )
+      ),
+      featuredMortgages: new FormArray(
+        this.homePage.featuredMortgages.map((d) => this.initFeaturedMortgage(d))
+      ),
+      mainCarousels: new FormArray(
+        this.homePage.mainCarousels.map((d) => this.initMainCarousel(d))
+      ),
       image: new FormControl(this.homePage.image),
       topCaptiom1: new FormControl(this.homePage.topCaptiom1),
       heading1: new FormControl(this.homePage.heading1),
@@ -34,31 +74,76 @@ export class AdminPanelComponent implements OnInit, DoCheck {
       topCaptiom2: new FormControl(this.homePage.topCaptiom2),
       heading2: new FormControl(this.homePage.heading2),
       bottomCaption2: new FormControl(this.homePage.bottomCaption2),
-      image2: new FormControl(this.homePage.image2),
-      topCaptiom3: new FormControl(this.homePage.topCaptiom3),
-      heading3: new FormControl(this.homePage.heading3),
-      image3: new FormControl(this.homePage.image3),
-      topCaptiom4: new FormControl(this.homePage.topCaptiom4),
-      heading4: new FormControl(this.homePage.heading4),
       video: new FormControl(this.homePage.video),
       mainHeading: new FormControl(this.homePage.mainHeading),
       subMainHeading: new FormControl(this.homePage.subMainHeading),
-      subHeading1: new FormControl(this.homePage.subHeading1),
-      subHeading2: new FormControl(this.homePage.subHeading2),
-      subHeading3: new FormControl(this.homePage.subHeading3),
-      subHeading4: new FormControl(this.homePage.subHeading4),
       image4: new FormControl(this.homePage.image4),
       paragraph: new FormControl(this.homePage.paragraph),
-      comments: new FormControl(this.homePage.comments),
-      names: new FormControl(this.homePage.names),
     });
+  }
+
+  initTestimonials(data: ITestimonial = {}) {
+    const form = new FormGroup({
+      comments: new FormControl(data.comments),
+      names: new FormControl(data.names),
+    });
+
+    return form;
+  }
+
+  initTipsForGettingAHomeMortgages(data: ITipsForGettingAHomeMortgage = {}) {
+    const form = new FormGroup({
+      heading: new FormControl(data.heading),
+      picture: new FormControl(data.picture),
+    });
+
+    return form;
+  }
+
+  initMainCarousel(data: IMainCarousel = {}) {
+    const form = new FormGroup({
+      header: new FormControl(data.header),
+      slider: new FormControl(data.slider),
+      subHeader: new FormControl(data.subHeader),
+    });
+
+    return form;
+  }
+
+  initFeaturedMortgage(data: IFeaturedMortgage = {}) {
+    const form = new FormGroup({
+      image2: new FormControl(data.image2),
+      topCaptiom3: new FormControl(data.topCaptiom3),
+      heading3: new FormControl(data.heading3),
+    });
+
+    return form;
+  }
+
+  addMainCarousel() {
+    this.mainCarousels.push(this.initMainCarousel());
+  }
+
+  addTestimonial() {
+    this.testimonials.push(this.initTestimonials());
   }
 
   setPageId(currentPage: number) {
     this.currentPage = currentPage;
   }
 
+  onSubmit() {}
+
   ngDoCheck() {
     console.log(this.HomePageForm);
+    console.log(this.homePage);
+    this.homePage.mainCarousels = this.HomePageForm.value.mainCarousels;
+    this.homePage.testimonials = this.HomePageForm.value.testimonials;
+    this.homePage.featuredMortgages = this.HomePageForm.value.featuredMortgages;
+    this.homePage.tipsForGettingAHomeMortgages = this.HomePageForm.value.tipsForGettingAHomeMortgages;
+  }
+
+  ConvertToInt(val: any) {
+    return parseInt(val);
   }
 }
--- angular/src/app/admin-panel/types.ts
+++ angular/src/app/admin-panel/types.ts
@@ -1,39 +1,57 @@
-export interface IHomePage {
-  // Top Carousel
-  sliders?: [];
+export interface ITestimonial {
+  comments?: string;
+  names?: string;
+}
+
+export interface ITipsForGettingAHomeMortgage {
+  picture?: string;
+  heading?: string;
+}
+
+export interface IFeaturedMortgage {
+  image2?: string;
+  topCaptiom3?: string;
+  heading3?: string;
+}
+
+export interface IMainCarousel {
+  slider?: string;
   header?: string;
   subHeader?: string;
+}
+
+export interface IHomePage {
+  // Top Carousel
+
+  mainCarousels?: IMainCarousel[];
   // Featured Mortgage Options Section 1
+
   image?: string;
   topCaptiom1?: string;
   heading1?: string;
   bottomCaption1?: string;
   // Featured Mortgage Options Section 2
+
   image1?: string;
   topCaptiom2?: string;
   heading2?: string;
   bottomCaption2?: string;
   // Featured Mortgage Options Section 2 Sub Section 1
-  image2?: string;
-  topCaptiom3?: string;
-  heading3?: string;
-  // Featured Mortgage Options Section 2 Sub Section 2
-  image3?: string;
-  topCaptiom4?: string;
-  heading4?: string;
+
+  featuredMortgages?: IFeaturedMortgage[];
   // Your Independent Mortgage Broker In California Section
+
   video?: string;
   // Tips For Getting A Home Mortgage In California Section 1
+
   mainHeading?: string;
   subMainHeading?: string;
-  subHeading1?: string;
-  subHeading2?: string;
-  subHeading3?: string;
-  subHeading4?: string;
+  tipsForGettingAHomeMortgages?: ITipsForGettingAHomeMortgage[];
   // Tips For Getting A Home Mortgage In California Section 2
+
   image4?: string;
   paragraph?: string;
+
   // Our Customer Says
-  comments?: [];
-  names?: [];
+  testimonials?: ITestimonial[];
 }
