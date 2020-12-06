19e191ce4621a000b276d3eaa8a58489c2cf7e29
KhalilMohammad <khalilmohammadmirza@gmail.com>
2020-12-06 22:28:48 +0500

    Credco api updates

--- aspnet-core/src/LoanManagement.Core/CoreLogicModels/CoreLogicModels/JointResponse/Creditor.cs
+++
@@ -1,9 +0,0 @@
-using System.Xml.Serialization;
-
-namespace LoanManagement.CoreLogicModels.JointResponse{
-
-[XmlRoot(ElementName="_CREDITOR")]
-	public class Creditor {
-		[XmlAttribute(AttributeName="_Name")]
-		public string _Name { get; set; }
-	}}
