4ee351d63d062184d5c6677c7f29e41a9821ccfb
shabir <shabir.abdulmajeed786@gmail.com>
2021-11-17 21:36:54 +0500

    worked on animated forms

---
+++ angular/src/app/Buy_a_Home_Loan _Options/lone-option-step10/lone-option-step10.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-lone-option-step10",
+  templateUrl: "./lone-option-step10.component.html",
+  styleUrls: ["./lone-option-step10.component.css"],
+})
+export class LoneOptionStep10Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
