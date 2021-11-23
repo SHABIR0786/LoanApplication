2e2ed49afbd58ef23360104af86e327998e18515
shabir <shabir.abdulmajeed786@gmail.com>
2021-11-23 22:45:05 +0500

    worked on animated forms

--- angular/src/app/Buy_a_Home_Loan _Options/lone-option-step2/lone-option-step2.component.ts
+++ angular/src/app/Buy_a_Home_Loan _Options/lone-option-step2/lone-option-step2.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Router } from "@angular/router";
 @Component({
   selector: "app-lone-option-step2",
   templateUrl: "./lone-option-step2.component.html",
   styleUrls: ["./lone-option-step2.component.css"],
 })
 export class LoneOptionStep2Component implements OnInit {
-  constructor() {}
+  constructor(private _route: Router) {}
 
   ngOnInit(): void {}
+  proceedToPrevious() {
+    this._route.navigate(["app/buy-a-home-loan-options-animated-step1"]);
+  }
 }
