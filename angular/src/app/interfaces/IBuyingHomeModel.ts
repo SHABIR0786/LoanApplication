1b1f1b414b83f2a61fdfb65f6f8db3884f5a37cc
shabir <shabir.abdulmajeed786@gmail.com>
2021-12-09 17:31:34 +0500

    buying a home

---
+++ angular/src/app/interfaces/IBuyingHomeModel.ts
@@ -0,0 +1,20 @@
+export interface IBuyingHomeModel {
+  id?: number;
+  PropertyType?: string;
+  PropertyUse?: string;
+  FirstTimeHomeBuying?: string;
+  planToPurchase?: string;
+  propertyLocated?: string;
+  purchasePrice?: string;
+  downPayment?: string;
+  currentEmployed?: number;
+  houseHoldIncome?: number;
+  proofOFincome?: string;
+  militarySevice?: string;
+  threeYears?: string;
+  twoYears?: string;
+  lastTowelMonths?: string;
+  rateYourCredit?: string;
+  emailAddress?: string;
+  phoneNumber?: string;
+}
