87d4caa8df616a5ba701bc669f229620cbf8e0d8
shabir.abdulmajeed786@gmail.com <shabir.abdulmajeed786@gmail.com>
2021-12-12 22:01:26 +0500

    worked on animated forms

--- angular/src/app/buy-a-home/animated-step2/animated-step2.component.ts
+++ angular/src/app/buy-a-home/animated-step2/animated-step2.component.ts
@@ -2,34 +2,32 @@ import { Result } from "common";
 import { IBuyingHomeModel } from "@app/interfaces/IBuyingHomeModel";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { HomeBuyingDataService } from "../../services/homeBuyingData.service";
+
 @Component({
   selector: "app-animated-step2",
   templateUrl: "./animated-step2.component.html",
   styleUrls: ["./animated-step2.component.css"],
 })
 export class AnimatedStep2Component implements OnInit {
-  constructor(private _route: Router) {}
+  formData: IBuyingHomeModel;
+  constructor(
+    private _route: Router,
+    private _homeBuyingDataService: HomeBuyingDataService
+  ) {}
   selected: number = 1;
   ngOnInit(): void {
-    const response: Result<IBuyingHomeModel> = JSON.parse(
-      localStorage.getItem("1")
-    );
-    this.selected = response.result.propertyUseId;
+    this.formData = this._homeBuyingDataService.data;
+    if (this.formData == null || this.formData == undefined) {
+      this._route.navigate(["app/buy-a-home-animated-step1"]);
+    }
   }
   proceedToPrevious() {
     this._route.navigate(["app/buy-a-home-animated-step1"]);
   }
   proceedToNext(value) {
-    var Form = localStorage.getItem("1");
-    if (Form) {
-      Form = JSON.parse(Form);
-      Form.propertyUseId = value;
-      localStorage.setItem("1", JSON.stringify(Form));
-    } else {
-      var newForm: Object = {};
-      newForm.propertyUseId = value;
-      localStorage.setItem("1", JSON.stringify(newForm));
-    }
+    this.formData.PropertyUse = value;
+    this._homeBuyingDataService.data = this.formData;
     this._route.navigate(["app/buy-a-home-animated-step3"]);
   }
 }
---
+++ aspnet-core/src/LoanManagement.Web.Core/Controllers/HomeBuyingController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Runtime.Validation;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using LoanManagement.Models;
+
+namespace LoanManagement.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class HomeBuyingController : LoanManagementControllerBase
+    {
+        [DisableValidation]
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] HomeBuying input)
+        {
+            try
+            {
+                if (input == null || !ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                //if (!input.Id.HasValue || input.Id.Value == default)
+                //{
+                //    input = await _loanAppService.CreateAsync(input);
+                //}
+
+                //await _loanAppService.UpdateAsync(input);
+
+                return Json(input);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message);
+            }
+        }
+    }
+}
