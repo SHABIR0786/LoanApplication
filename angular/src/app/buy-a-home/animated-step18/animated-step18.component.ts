198efa6e94018b38ecfd899663fab5fd2ce83d74
Aqeel Abdulmajeed <aqeel.abdulmajeed786@gmail.com>
2021-12-13 01:28:32 +0500

    buying-a-home-guide-steps

--- angular/src/app/buy-a-home/animated-step18/animated-step18.component.ts
+++ angular/src/app/buy-a-home/animated-step18/animated-step18.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { IBuyingHomeModel } from "@app/interfaces/IBuyingHomeModel";
+import { HomeBuyingDataService } from "../../services/homeBuyingData.service";
 @Component({
   selector: "app-animated-step18",
   templateUrl: "./animated-step18.component.html",
   styleUrls: ["./animated-step18.component.css"],
 })
 export class AnimatedStep18Component implements OnInit {
-  constructor(private _route: Router) {}
+  formData: IBuyingHomeModel;
+  constructor(
+    private _route: Router,
+    private _homeBuyingDataService: HomeBuyingDataService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formData = this._homeBuyingDataService.data;
+    if (this.formData == null || this.formData == undefined) {
+      this._route.navigate(["app/buy-a-home-animated-step1"]);
+    }
+  }
   proceedToPrevious() {
     this._route.navigate(["app/buy-a-home-animated-step17"]);
   }
-  proceedToNext() {
+  proceedToNext(value) {
+    this.formData.phoneNumber = value;
+    this._homeBuyingDataService.data = this.formData;
     this._route.navigate(["app/buy-a-home-animated-step19"]);
   }
 }
--- angular/src/app/buy-a-home/animated-step19/animated-step19.component.ts
+++ angular/src/app/buy-a-home/animated-step19/animated-step19.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { IBuyingHomeModel } from "@app/interfaces/IBuyingHomeModel";
+import { HomeBuyingService } from "../../services/home-buying.service";
+import { HomeBuyingDataService } from "../../services/homeBuyingData.service";
 @Component({
   selector: "app-animated-step19",
   templateUrl: "./animated-step19.component.html",
   styleUrls: ["./animated-step19.component.css"],
 })
 export class AnimatedStep19Component implements OnInit {
-  constructor(private _route: Router) {}
+  formData: IBuyingHomeModel;
+  constructor(
+    private _route: Router,
+    private _homeBuyingService: HomeBuyingService,
+    private _homeBuyingDataService: HomeBuyingDataService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formData = this._homeBuyingDataService.data;
+    if (this.formData == null || this.formData == undefined) {
+      this._route.navigate(["app/buy-a-home-animated-step1"]);
+    }
+  }
   proceedToPrevious() {
     this._route.navigate(["app/buy-a-home-animated-step18"]);
+  }
+  SubmitForm(value) {
+    this.formData.refferedBy = value;
+    this._homeBuyingDataService.data = this.formData;
+    console.log(this._homeBuyingDataService.data);
+    console.log(this.formData);
+
+    // this._homeBuyingService
+    // .post<Result<IBuyingHomeModel>>("Add", formData)
+    // .subscribe(
+    //   (response) => {
+    //     console.log(response);
+    //     // this._dataService.loanApplication = response.result;
+    //     // this.form.patchValue(this._dataService.loanApplication.loanDetails);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+
+    // this._route.navigate(["app/home"]);
   }
 }
