7b50b342674b595c3e650a70adbc79e9f2c1bcbe
Muhammad Hanif <muhammadhanifabbasi@hotmail.com>
2022-04-24 02:09:14 +0500

    added style
    addedd style

---
+++ angular/src/app/modules/purchase/index.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+const ROUTE_DATA = [
+  "/purchase/welcome",
+  "/purchase/property-info",
+  "/purchase/income-info",
+  "/purchase/assets-info",
+  "/purchase/gov",
+];
+const TABS_DATA = [];
+@Component({
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"],
+})
+export class IndexComponent implements OnInit {
+  a: number = 0;
+  constructor(private router: Router) {
+    this.router.events.subscribe((x) => {
+      if (x instanceof NavigationEnd) {
+        if (x.urlAfterRedirects === "/app/purchase") {
+          this.a = 0;
+        } else {
+          ROUTE_DATA.filter((obj, i) => {
+            if (new RegExp(obj + "(.*?)").test(x.urlAfterRedirects)) {
+              this.a = i + 1;
+            }
+          });
+        }
+      }
+    });
+  }
+
+  ngOnInit() {}
+}
