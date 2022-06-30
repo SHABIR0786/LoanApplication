fdb38d4e87eb3f5ffbf2eb1ffa13162f46a0d74f
Abdul Haseeb <abdulhaseeb.logicStack@gmail.com>
2022-06-30 23:51:14 +0500

    Upload Loan app document

---
+++ aspnet-core/src/LoanManagement.Core/Features/AdminLoanApplicationDocument/UploadAdminLoanApplicationDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagement.Features.AdminLoanApplicationDocument
+{
+    public class UploadAdminLoanApplicationDocument
+    {
+        public int LoanId { get; set; }
+        public int DisclosureId { get; set; }
+        public int UserId { get; set; }
+    }
+}
