44dc0533dc2d50e082d3ccf612bd877dbfb1d8f2
Shailaja Avaiya <shailaja@Shailajas-MacBook-Air.local>
2022-07-15 22:08:09 +0530

    purchase & refinance issues

--- angular/src/shared/directives/phone-mask.directive.ts
+++ angular/src/shared/directives/phone-mask.directive.ts
@@ -1,40 +1,38 @@
-import { Directive, HostListener } from '@angular/core';
-import { NgControl } from '@angular/forms';
+import { Directive, HostListener } from "@angular/core";
+import { NgControl } from "@angular/forms";
 
 @Directive({
-  selector: '[formControlName][appPhoneMask]',
+  selector: "[appPhoneMask]",
 })
 export class PhoneMaskDirective {
+  constructor(public ngControl: NgControl) {}
 
-  constructor(public ngControl: NgControl) { }
-
-  @HostListener('keydown', ['$event'])
+  @HostListener("keydown", ["$event"])
   onModelChange(event) {
     this.onInputChange(event.target.value, false);
   }
 
-  @HostListener('keydown.backspace', ['$event'])
+  @HostListener("keydown.backspace", ["$event"])
   keydownBackspace(event) {
     this.onInputChange(event.target.value, true);
   }
-  
 
   onInputChange(event, backspace) {
-    let newVal = event.replace(/\D/g, '');
+    let newVal = event.replace(/\D/g, "");
     if (backspace && newVal.length <= 6) {
       newVal = newVal.substring(0, newVal.length - 1);
     }
     if (newVal.length === 0) {
-      newVal = '';
+      newVal = "";
     } else if (newVal.length <= 3) {
-      newVal = newVal.replace(/^(\d{0,3})/, '($1)');
+      newVal = newVal.replace(/^(\d{0,3})/, "($1)");
     } else if (newVal.length <= 6) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) $2');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, "($1) $2");
     } else if (newVal.length <= 10) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
     } else {
       newVal = newVal.substring(0, 10);
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, "($1) $2-$3");
     }
     this.ngControl.valueAccessor.writeValue(newVal);
   }
