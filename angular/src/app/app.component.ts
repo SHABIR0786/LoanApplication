1af73f0b341a0f45fe6d5d60446f22c680200851
Gautam <gautam.techconsultant@gmail.com>
2022-09-05 20:10:26 +0530

    realtime

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -218,22 +218,9 @@ export class AppComponent extends AppComponentBase implements OnInit {
   ngOnInit(): void {
     this.renderer.addClass(document.body, "sidebar-mini");
 
-    SignalRAspNetCoreHelper.initSignalR();
+    
 
-    abp.event.on("abp.notifications.received", (userNotification) => {
-      abp.notifications.showUiNotifyForUserNotification(userNotification);
-
-      // Desktop notification
-      Push.create("AbpZeroTemplate", {
-        body: userNotification.notification.data.message,
-        icon: abp.appPath + "assets/app-logo-small.png",
-        timeout: 6000,
-        onClick: function () {
-          window.focus();
-          this.close();
-        },
-      });
-    });
+    
 
     this._layoutStore.sidebarExpanded.subscribe((value) => {
       this.sidebarExpanded = value;
