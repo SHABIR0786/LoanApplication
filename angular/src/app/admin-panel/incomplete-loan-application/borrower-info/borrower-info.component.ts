aa004929f1ec06a2830fc2ad3c823d49ea54e0db
SaaimaGul <62097501+SaaimaGul@users.noreply.github.com>
2023-06-08 19:43:04 +0500

    Add files via upload

--- angular/src/app/admin-panel/incomplete-loan-application/borrower-info/borrower-info.component.ts
+++ angular/src/app/admin-panel/incomplete-loan-application/borrower-info/borrower-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import {Address, AlternateNames, BorrowModel, CreditList, Employment, GrossMonthlyIncome, IncomeOtherSource, PersonalInformation, Source} from './borrower-model';
+import { LoanManagementService } from "@shared/service/loanmanagement.service";
+import { BorrowService } from "app/services/borrow.service";
+
 
 @Component({
   selector: "app-borrower-info",
@@ -6,7 +10,62 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./borrower-info.component.css"],
 })
 export class BorrowerInfoComponent implements OnInit {
-  constructor() {}
+  borrowerInfo: BorrowModel= new BorrowModel();
+  constructor(private loanManagmentService: LoanManagementService, private borrowService:BorrowService) {
+    this.borrowerInfo.personalInformation = new PersonalInformation();
+    this.borrowerInfo.personalInformation.alternateNames = new AlternateNames();
+    this.borrowerInfo.personalInformation.address = [];
+    this.borrowerInfo.personalInformation.address.push(new Address());
+    this.borrowerInfo.personalInformation.address.push(new Address());
+    this.borrowerInfo.personalInformation.address.push(new Address());
+    this.borrowerInfo.employment = [];
+    this.borrowerInfo.employment.push(new Employment()); // employement
+    this.borrowerInfo.employment[0].grossMonthlyIncome = new GrossMonthlyIncome();
+    this.borrowerInfo.employment.push(new Employment()); //additional employement
+    this.borrowerInfo.employment[1].grossMonthlyIncome = new GrossMonthlyIncome();
+    this.borrowerInfo.employment.push(new Employment()); //current or previous employement
+    this.borrowerInfo.employment[2].grossMonthlyIncome = new GrossMonthlyIncome();
+    this.borrowerInfo.incomeOtherSources =[];
+    this.borrowerInfo.incomeOtherSources.push(new IncomeOtherSource());
+    // this.borrowerInfo.incomeOtherSources.push(new IncomeOtherSource());
+    // this.borrowerInfo.incomeOtherSources.push(new IncomeOtherSource());
+    this.borrowerInfo.incomeOtherSources[0].sources = [];
+    this.borrowerInfo.incomeOtherSources[0].sources.push(new Source());
 
-  ngOnInit(): void {}
+  }
+
+  ngOnInit(): void {
+    this.creditClick();
+    // this.borrowService.getAllCitizenshipType().subscribe(
+    //   (res: any) => {
+    //     console.log(res.result);
+    //     //this.Custom = res.result.items;
+    //   }
+    // );
+  }
+  creditClick(){
+    debugger;
+    if(this.borrowerInfo.personalInformation.creditValue == "1"){
+      this.borrowerInfo.personalInformation.totalBorrowers = 1;
+    }
+    this.borrowerInfo.personalInformation.creditList=[];
+    for(var i =0; i<this.borrowerInfo.personalInformation.totalBorrowers ; i++ ){
+      this.borrowerInfo.personalInformation.creditList.push(new CreditList());
+    }
+  }
+  addMoreIncomeSource(){
+    this.borrowerInfo.incomeOtherSources[0].sources.push(new Source());
+  }
+  delMoreIncomeSource(){
+    this.borrowerInfo.incomeOtherSources[0].sources.splice(-1);
+  }
+  nextBtnClick(){
+    console.log(this.borrowerInfo);
+    this.borrowService.createMortgageLoanApplication(this.borrowerInfo).subscribe(
+         (res: any) => {
+        console.log(res.result);
+        alert("data has inserted successfully");
+      }
+    );
+  }
 }
